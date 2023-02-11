c7c367f18c28ccfb2d68e50e889152f56834dd33
qwotic <qwoticreator@gmail.com>
2023-02-11 18:37:38 +0700

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Данил Белов on 11.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
